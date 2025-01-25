33058b1ace14fd4ebc59e9e79e7bf9ced8784d14
dharmicreddy <mdharmicreddy@gmail.com>
2025-01-25 11:06:44 -0600

    Commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,6 +25,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # Directory for development static files
+STATICFILES_DIRS = [BASE_DIR / 'Base/static']  # Directory for development static files
 STATIC_ROOT = BASE_DIR / 'staticfiles'   # Directory for collected static files
 
 
