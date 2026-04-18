1dd75eba78ff42912a1f8cb8ee25f8cd51271578
dharmicreddy <152347802+dharmicreddy@users.noreply.github.com>
2026-04-18 10:30:15 -0500

    Delete Base directory

--- Base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-urlpatterns = [
-    path('',views.home, name='home'),
-    path('experience/', views.experience, name='experience'),
-    path('projects/', views.projects, name='projects'),
-    path('skills/', views.skills, name='skills'),
-    path('Education/', views.Education, name='Education'),
-]
